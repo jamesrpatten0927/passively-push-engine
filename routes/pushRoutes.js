d9b63b7c815c6f468eec17bb019b514c08086c72
jamesrpatten0927 <james.r.patten@gmail.com>
2026-05-22 21:08:18 -0500

    Replace requireAuth with authenticateUser middleware

--- routes/pushRoutes.js
+++ routes/pushRoutes.js
@@ -1,29 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const pushController = require("../controllers/pushController");
-
-const requireAuth = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (
-    !authHeader ||
-    authHeader !== `Bearer ${process.env.ADMIN_PASSWORD}`
-  ) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  next();
-};
+const { authenticateUser } = require("../middleware/auth");
 
 router.post("/subscribe", pushController.subscribe);
+
 router.post(
   "/send-notification",
-  requireAuth,
+  authenticateUser,
   pushController.sendNotification
 );
+
 router.get(
   "/subscribers",
-  requireAuth,
+  authenticateUser,
   pushController.getSubscribers
 );
 
