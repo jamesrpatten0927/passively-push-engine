55e9c35d67d603f9abf7ec236cc03a677a8ec716
jamesrpatten0927 <james.r.patten@gmail.com>
2026-05-25 02:16:50 -0500

    Refactor panelRoutes.js for cleaner syntax

--- routes/panelRoutes.js
+++ routes/panelRoutes.js
@@ -1,4 +1,4 @@
-const express = require(“express”);
+const express = require("express");
 
 const router = express.Router();
 
@@ -8,12 +8,7 @@ ROOT TEST
 
 */
 
-router.get(”/”, (req, res) => {
-return res.json({
-success: true,
-message: “Panels API working”
-});
-});
+router.get("/", (req, res) => {return res.json({success: true,message: "Panels API working"});});
 
 /*
 
@@ -21,17 +16,10 @@ GET PANEL
 
 */
 
-router.get(”/:panelId”, (req, res) => {
+router.get("/:panelId", (req, res) => {
 
 const panelId = req.params.panelId;
 
-return res.json({
-success: true,
-panel: {
-id: panelId,
-title: “Dynamic Panel Route Working”
-}
-});
-});
+return res.json({success: true,panel: {id: panelId,title: "Dynamic Panel Route Working"}});});
 
 module.exports = router;
