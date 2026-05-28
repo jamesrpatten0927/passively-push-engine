9bf1015f62f168267cde4695952b9f86a5c4c19b
jamesrpatten0927 <james.r.patten@gmail.com>
2026-05-28 16:24:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
-const express = require("express");
+require(“dotenv”).config();
+
+const express = require(“express”);
+const cors = require(“cors”);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server running");
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+/*
+
+HEALTH
+
+*/
+
+app.get(”/”, (req, res) => {
+res.send(“Passively Push Engine Running”);
 });
 
-app.listen(3001, () => {
-  console.log("Running");
+app.get(”/health”, (req, res) => {
+
+res.json({
+status: “ok”
+});
+
+});
+
+/*
+
+TEST PUSH ROUTE
+
+*/
+
+app.post(”/api/test”, (req, res) => {
+
+console.log(“TEST ROUTE HIT”);
+
+res.json({
+success: true
+});
+
+});
+
+/*
+
+START SERVER
+
+*/
+
+app.listen(PORT, () => {
+
+console.log(Server running on ${PORT});
+
 });
