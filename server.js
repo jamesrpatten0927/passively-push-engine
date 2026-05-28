23095763ab98f1ffd6ac4ff5cec79e4ac5506110
jamesrpatten0927 <james.r.patten@gmail.com>
2026-05-28 17:35:27 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+
+  res.send("WORKING");
+
+});
+
+app.listen(3001, function() {
+
+  console.log("RUNNING");
+
+});
