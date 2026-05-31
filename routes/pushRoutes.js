59a4fe3d3be38d6281ea0fc1fdb0944380c43ee0
jamesrpatten0927 <james.r.patten@gmail.com>
2026-05-31 17:47:55 -0500

    Update pushRoutes.js

--- routes/pushRoutes.js
+++ routes/pushRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const pushController = require('../controllers/pushController');
 const { authenticateUser } = require('../middleware/auth');
+
 // Public route - NO JWT REQUIRED for visitors
 router.post('/subscribe', pushController.subscribe);
+
 // Protected routes - Require JWT (authenticateUser) for dashboard owner
 router.get('/subscribers/:userId', authenticateUser, pushController.getSubscribers);
 router.post('/send-notification', authenticateUser, pushController.sendNotification);
+
 module.exports = router;
