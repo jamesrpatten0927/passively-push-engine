4f58afe186b4817bf1adc362cd961c2bda0cbaa2
jamesrpatten0927 <james.r.patten@gmail.com>
2026-06-02 20:03:50 -0500

    Replace advisorId with panelId in campaign routes

--- routes/campaignRoutes.js
+++ routes/campaignRoutes.js
@@ -5,17 +5,17 @@ const { authenticateUser } = require('../middleware/auth');
 
 // Public Endpoint (Used by widget.js Spotlight Runtime)
 // No JWT required, read-only
-router.get('/spotlights/public/:advisorId', campaignController.getPublicCampaigns);
+router.get('/spotlights/public/:panelId', campaignController.getPublicCampaigns);
 
 // Protected Endpoints (Used by Advisor Studio / Campaign Center)
 // Requires JWT
 router.use('/campaigns', authenticateUser);
-router.get('/campaigns/:advisorId', campaignController.getCampaigns);
+router.get('/campaigns/:panelId', campaignController.getCampaigns);
 router.post('/campaigns', campaignController.saveCampaign);
 router.delete('/campaigns/:id', campaignController.deleteCampaign);
 
 router.use('/schedules', authenticateUser);
-router.get('/schedules/:advisorId', campaignController.getSchedules);
+router.get('/schedules/:panelId', campaignController.getSchedules);
 router.post('/schedules', campaignController.saveSchedule);
 router.delete('/schedules/:id', campaignController.deleteSchedule);
 
