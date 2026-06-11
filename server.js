9cadd4e6ea3538e2eed5320479ad7019b112faf7
jamesrpatten0927 <james.r.patten@gmail.com>
2026-06-10 21:01:23 -0500

    Add scanRoutes to server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const spotlightRoutes = require("./routes/spotlightRoutes");
 const leadRoutes = require("./routes/leadRoutes");
 const ownerSubscriptionRoutes = require("./routes/ownerSubscriptionRoutes");
 const authRoutes = require("./routes/authRoutes");
+const scanRoutes = require('./routes/scanRoutes');
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use("/api/spotlights", spotlightRoutes);
 app.use("/api/leads", leadRoutes);
 app.use("/api/owner-subscriptions", ownerSubscriptionRoutes);
 app.use("/api/auth", authRoutes);
+app.use('/api/scans', scanRoutes);
 
 /*
 WEB PUSH CONFIG
