bb2ddd850f60861b713c0c8926b72aa7d3730fb0
jamesrpatten0927 <james.r.patten@gmail.com>
2026-07-01 11:09:15 -0500

    Update spotlightEvents.js

--- constants/spotlightEvents.js
+++ constants/spotlightEvents.js
@@ -1,1 +1,21 @@
+const EVENT_TYPES = {
+  SHOWN: 'shown',
+  ANSWERED: 'answered',
+  DISMISSED: 'dismissed',
+  MINIMIZED: 'minimized',
+  CTA_CLICKED: 'cta_clicked',
+  COMPLETED: 'completed',
+  OPTED_IN: 'opted_in'
+};
 
+const SPOTLIGHT_TYPES = {
+  GENERAL: 'general',
+  INTENT: 'intent',
+  INTENT_POLL: 'intent_poll',
+  CONTEXTUAL_OPTIN: 'contextual_optin'
+};
+
+module.exports = {
+  EVENT_TYPES,
+  SPOTLIGHT_TYPES
+};
