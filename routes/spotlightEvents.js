b5b97ff404aacee2ce402052328180bc48a9d28e
jamesrpatten0927 <james.r.patten@gmail.com>
2026-07-01 11:12:54 -0500

    Update spotlightEvents.js

--- routes/spotlightEvents.js
+++ routes/spotlightEvents.js
@@ -1,8 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { recordEvent } = require('../controllers/spotlightEventsController');
 
-router.post("/", async (req, res) => {
-
-});
+router.post('/events', recordEvent);
 
 module.exports = router;
